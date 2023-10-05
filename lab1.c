9b2f6a7ea4d7255cdacf70547e996315a05c81aa
Nisarg70968 <146726749+Nisarg70968@users.noreply.github.com>
2023-10-05 18:28:06 +0530

    lab1
    update file

---
+++ lab1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    //Basic information Answer 1.1
+    printf("Name: Nisarg D. Patel\n");
+    printf("Age: 23\n");
+    printf("Occupation: Student\n");
+    printf("Location: Surat,Gujarat\n");
+    printf("Contect Num: 7096814375 \n");
+    printf("Email: nisargpatel7096@gmail.com \n");
+
+    return 0;
+}
